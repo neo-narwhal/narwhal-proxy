f5124d622147bc17aa22000189cb1e0206751540
yaoandy107 <yaoandy107@gmail.com>
2019-10-16 21:12:45 +0800

    Fix router bug

--- app/containers/__init__.py
+++ app/containers/__init__.py
@@ -12,10 +12,11 @@ blueprint = Blueprint('containers', __name__)
 api = Api(blueprint)
 
 
-@api.route('/p/<project_name>', subdomain='<username>')
+@api.route('', subdomain='<username>')
 class Containers(Resource):
-    def get(self, project_name, username):
+    def get(self, username):
         print('Containers GET')
+        project_name = request.args.get('p')
         user = User.query.filter(User.username == username).first()
         if user:
             project = Project.query.filter(and_(Project.user_id == user.id,
@@ -34,24 +35,32 @@ class Containers(Resource):
             return Response('', status=403)
 
 
-@api.route('/p/<project_name>/<code>', subdomain='<username>')
+@api.route('/<code>', subdomain='<username>')
 class Containers(Resource):
-    def get(self, project_name, code, username):
+    def get(self, code, username):
         print('Containers GET')
-        user = User.query.filter(User.username == username).first()
-        if user:
-            project = Project.query.filter(and_(Project.user_id == user.id,
-                                                Project.name == project_name)).first()
-            if project:
-                data = requests.get('http://localhost:{}/{}'.format(project.port, code))
-                reponse = Response(data, status=200)
-                reponse.set_cookie('np', json.dumps({
-                    'username': username,
-                    'project_name': project_name
-                }))
-                return reponse
+        cookie = request.cookies['np']
+        if cookie:
+            user_info = json.loads(cookie)
+            username = user_info['username']
+            project_name = user_info['project_name']
+            print(username, project_name)
+            user = User.query.filter(User.username == username).first()
+            if user:
+                project = Project.query.filter(and_(Project.user_id == user.id,
+                                                    Project.name == project_name)).first()
+                if project:
+                    data = requests.get('http://localhost:{}/{}'.format(project.port, code))
+                    reponse = Response(data, status=200)
+                    reponse.set_cookie('np', json.dumps({
+                        'username': username,
+                        'project_name': project_name
+                    }))
+                    return reponse
+                else:
+                    return Response('', status=404)
             else:
-                return Response('', status=404)
+                return Response('', status=403)
         else:
             return Response('', status=403)
 
