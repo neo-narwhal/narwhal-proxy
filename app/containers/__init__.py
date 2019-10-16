a6df39542d24f1d578fe55ffe810d37810b2c38d
yaoandy107 <yaoandy107@gmail.com>
2019-10-16 20:58:30 +0800

    Fix router bug

--- app/containers/__init__.py
+++ app/containers/__init__.py
@@ -34,6 +34,28 @@ class Containers(Resource):
             return Response('', status=403)
 
 
+@api.route('/p/<project_name>/<code>', subdomain='<username>')
+class Containers(Resource):
+    def get(self, project_name, code, username):
+        print('Containers GET')
+        user = User.query.filter(User.username == username).first()
+        if user:
+            project = Project.query.filter(and_(Project.user_id == user.id,
+                                                Project.name == project_name)).first()
+            if project:
+                data = requests.get('http://localhost:{}/{}'.format(project.port, code))
+                reponse = Response(data, status=200)
+                reponse.set_cookie('np', json.dumps({
+                    'username': username,
+                    'project_name': project_name
+                }))
+                return reponse
+            else:
+                return Response('', status=404)
+        else:
+            return Response('', status=403)
+
+
 @api.route('/<path:dummy>', subdomain='<username>')
 class Static(Resource):
     def get(self, dummy, username):
