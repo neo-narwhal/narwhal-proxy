2c5aaeffba055446dbc0757ac7451868303fdd83
yaoandy107 <yaoandy107@gmail.com>
2019-10-16 11:39:55 +0000

    Fix 404 bug

--- app/containers/__init__.py
+++ app/containers/__init__.py
@@ -14,12 +14,13 @@ api = Api(blueprint)
 @api.route('/<project_name>', subdomain='<username>')
 class Containers(Resource):
     def get(self, project_name, username):
+        print('Containers GET')
         user = User.query.filter(User.username == username).first()
         if user:
             project = Project.query.filter(and_(Project.user_id == user.id,
                                                 Project.name == project_name)).first()
             if project:
-                data = requests.get('http://localhost:{}'.format(project.port)).content()
+                data = requests.get('http://localhost:{}'.format(project.port))
                 return Response(data, status=200)
             else:
                 return Response('', status=404)
@@ -27,21 +28,22 @@ class Containers(Resource):
             return Response('', status=403)
 
 
-@api.route('/<path:dummy>', subdomain='*')
+@api.route('/<path:dummy>', subdomain='<username>')
 class Static(Resource):
-    def get(self, dummy):
+    def get(self, dummy, username):
+        print('Static GET')
         referer = request.headers.get("Referer")
         if referer:
             pattern = re.compile(r'^https?://(.*)\.narwhal\.ntut\.club/(.*)$')
             m = pattern.match(referer)
             username, project_name = m.group(1), m.group(2)
-
+            print(username, project_name)
             user = User.query.filter(User.username == username).first()
             if user:
                 project = Project.query.filter(and_(Project.user_id == user.id,
                                                     Project.name == project_name)).first()
                 if project:
-                    data = requests.get('http://localhost:{}'.format(project.port)).content().replace('./.*', ':/')
+                    data = requests.get('http://localhost:{}'.format(project.port))
                     return Response(data, status=200)
                 else:
                     return Response('', status=404)
